e9daad68c47a27c9d3e516c72bffbc65c251a42c
msai1 <medicherladurgasai@gamil.com>
2025-06-05 07:52:04 +0530

    definition change

--- app/src/main/java/com/example/deeplinking/DeepLinking.kt
+++ app/src/main/java/com/example/deeplinking/DeepLinking.kt
@@ -3,15 +3,8 @@ package com.example.deeplinking
 /*
  * ✅ What is Deep Linking in Android?
  *
- * Deep linking allows users (or external apps) to navigate directly to a **specific screen or destination**
+ * Deep linking allows users to navigate directly to a **specific screen or destination**
  * within your app using a **URL or URI** (e.g., https://example.com/profile/123).
- *
- * 🔹 It provides seamless app navigation from:
- *    - Websites
- *    - Emails
- *    - SMS
- *    - Notifications
- *    - Other apps
  *
  * 🔹 It enhances user experience by:
  *    - Opening the relevant content directly inside the app
